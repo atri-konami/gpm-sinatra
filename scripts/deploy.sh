2acf2882fdf772590b9603791bacbcbc96ec33f3
attribute_k <freude1994@gmail.com>
2018-04-17 14:15:02 +0900

    move shell scripts to scripts/

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,4 @@
+cd $(dirname $0)/../
 mkdir -p tmp/pids
 mkdir -p log
 bundle exec unicorn -E production -c unicorn.rb -D
--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,4 +1,4 @@
-cd $(dirname $0)
+cd $(dirname $0)/../
 kill -QUIT `cat tmp/pids/unicorn.pid`
 sleep 3 && ps aux | grep unicorn
 
